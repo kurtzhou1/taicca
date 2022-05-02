2e6393bb738065c38d7e930ded5d917383407414
KurtZhou <pandabat1@gmail.com>
2022-05-02 12:09:13 +0800

    [dev]page3 done

--- src/components/Comic.jsx
+++ src/components/Comic.jsx
@@ -4,7 +4,24 @@ import './styles/page_black.scss'
 const Comic = () => {
     
     return (
-        <div className="focus" />
+        <div className="comic">
+            <div className="block1">
+                <div className="title">
+                    韓國網漫進化史 → Webtoon產業創造新商模
+                </div>
+                <div className="content">
+                    韓國Webtoon(網路漫畫)產業被視為繼韓劇(K-Drama)、韓國流行音樂(K-POP)之後，
+                    有潛力成為下一個推廣韓國文化到國際舞台的韓流產業。Webtoon一字由網路媒介「Web」
+                    與漫畫「cartoon」兩詞結合而成，專指載於網路平台上的漫畫。當Webtoon逐漸取代傳統紙本漫畫
+                    成為創造韓國漫畫產業值的主力，韓國漫畫產業的整體生態隨之改變，無論是產業發展走向、產業角色
+                    重組、政策規劃、行業標準接軌Webtoon產業的未來發展。
+                </div>
+            </div>
+            <div className="block2">
+                根據KOCCA的統計觀察，韓國網路漫畫新作品於2013年至2016年的產量，
+                從原本的382部增加到1,971部，短短三年內突增了1,589部，成長超過五倍。
+            </div>
+        </div>
     );
 }
 
